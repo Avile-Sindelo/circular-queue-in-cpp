2e6c78ded232a746750f6d848bb7d7a1bb2ab2e2
Avile Sindelo <avilesindelo@gmail.com>
2023-09-22 12:42:11 +0200

    enqueue and dequeue psuedocodes

--- CQueue.cpp
+++ CQueue.cpp
@@ -1,4 +1,4 @@
-#include<iostrem>
+#include<iostream>
 #define MAX 100
 using namespace std;
 
@@ -16,5 +16,76 @@ class CQueue {
 			void enqueue(char ch);
 			char dequeue();
 };
+
+CQueue::CQueue(){
+	size = 0;
+}
+
+bool CQueue::isFull(){
+	return (size == MAX);
+}
+
+bool CQueue::isEmpty(){
+	return (size == 0);
+}
+
+//ENQUEUE
+void CQueue::enqueue(char character){
+	//if the queue is full
+		//cannot enqueue
+	//else if the queue is empty
+		//front = back = 0;
+		//Add the new character at queue[back]
+	//else (NOT empty && NOT full)
+		//Increment the Back
+		//if Back == MAX
+			//Back = 0
+		//Add the new character at queue[back]
+		//Increment the Size by 1
+}
+
+//DEQUEUE
+char CQueue::dequeue(){
+	//if the queue is empty
+		//CANNOT dequeue - queue is empty
+	//else  (There's something in the queue)
+		//declare a variable to hold the dequeued character
+		//store the value of queue[front] into the new variable declared above
+		//increment front by 1
+		//if front==MAX
+			//front = 0
+		//Decrement the size by 1
+		//Return the dequeued variable
+}
+
+
+int main(){
+	CQueue circle;	
+	
+	
+	
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
